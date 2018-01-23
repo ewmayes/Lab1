5347b1eddec2407047c85c471babd36799f6d13d
Everett Mayes <ewmayes@hotmail.com>
2018-01-23 15:44:29 -0500

    I forgot to prompt the user to do it again

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -10,33 +10,44 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            // Input
+            string Answer;
+            do
+            {
 
-            double Width;
-            double Length;
-            double Height;
+                // Input
 
-            Console.WriteLine("Please enter the width");
-            Width = double.Parse(Console.ReadLine());
+                double Width;
+                double Length;
+                double Height;
 
-            Console.WriteLine("Please enter the length");
-            Length = double.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter the width");
+                Width = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Please enter the height");
-            Height = double.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter the length");
+                Length = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter the height");
+                Height = double.Parse(Console.ReadLine());
+
+                
 
 
-            // Processing
 
-            double Area = Width * Length;
-            double Perimeter = 2 * Width + 2 * Length;
-            double Volume = Width * Length * Height;
+                // Processing
 
-            // Output
+                double Area = Width * Length;
+                double Perimeter = 2 * Width + 2 * Length;
+                double Volume = Width * Length * Height;
 
-            Console.WriteLine("The area is " + Area + " units squared and the perimeter is " + Perimeter + " units.");
-            Console.WriteLine("The volume is " + Volume + " units cubed.");
+                // Output
 
+                Console.WriteLine("The area is " + Area + " units squared and the perimeter is " + Perimeter + " units.");
+                Console.WriteLine("The volume is " + Volume + " units cubed.");
+
+                Console.WriteLine("Would you like to continue?");
+                Answer = Console.ReadLine();
+            }
+            while (Answer == "Y");
         }
     }
 }
