39564d103333a2a557a2d60fa80dbe3b96a98fc8
Everett Mayes <ewmayes@hotmail.com>
2018-01-23 14:44:48 -0500

    Lab 1

---
+++ Lab1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input
+
+            double Width;
+            double Length;
+            double Height;
+
+            Console.WriteLine("Please enter the width");
+            Width = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the length");
+            Length = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the height");
+            Height = double.Parse(Console.ReadLine());
+
+
+            // Processing
+
+            double Area = Width * Length;
+            double Perimeter = 2 * Width + 2 * Length;
+            double Volume = Width * Length * Height;
+
+            // Output
+
+            Console.WriteLine("The area is " + Area + " units squared and the perimeter is " + Perimeter + " units.");
+            Console.WriteLine("The volume is " + Volume + " units cubed.");
+
+        }
+    }
+}
